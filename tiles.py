a7ec3856c82eed6782f0a1932d047437692cfe89
Alexander Stefánsson <allisnaer@gmail.com>
2019-09-17 14:21:26 +0000

    test

--- tiles.py
+++ tiles.py
@@ -23,14 +23,15 @@ def direction():
 
 def movement(take_direction):
     while True:
-        if input == 'n' or input == 'N':
+        if take_direction == 'n' or take_direction == 'N':
             y += 1
-        elif input == 'e' or input == 'E':
+        elif take_direction == 'e' or take_direction == 'E':
             x += 1
-        elif input == 's' or input == 'S':
+        elif take_direction == 's' or take_direction == 'S':
             y -= 1
-        elif input == 'w' or input == 'W':
+        elif take_direction == 'w' or take_direction == 'W':
             x -= 1
         else:
             print('“Not a valid direction!')
+            direction()
 
