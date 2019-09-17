13b27911eb2fbe66a7af2a5fc4ba36ad1598c9a2
Alexander Stefánsson <allisnaer@gmail.com>
2019-09-17 14:16:13 +0000

    added function

--- tiles.py
+++ tiles.py
@@ -1,6 +1,36 @@
-def direction (xy):
+#Heldur utan um stöðu player
+
+#Prentar möguleika
+
+def prints_options():
+    print('You can travel' + str())
 
 
+#Taka input um hvað user vill gera
+
+
+
+print('Lol')
 
 direction = input("where do you want to go ?")
-    
+
+x = 1
+y = 1
+
+def direction():
+    take_direction = input('Direction: ')
+    return take_direction
+
+def movement(take_direction):
+    while True:
+        if input == 'n' or input == 'N':
+            y += 1
+        elif input == 'e' or input == 'E':
+            x += 1
+        elif input == 's' or input == 'S':
+            y -= 1
+        elif input == 'w' or input == 'W':
+            x -= 1
+        else:
+            print('“Not a valid direction!')
+
